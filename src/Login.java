82e89d97b1b663e7db88c2556b0148219eb12fca
millenito <wownito31@gmail.com>
2020-05-20 19:14:08 +0700

    Login view done

--- src/Login.java
+++ src/Login.java
@@ -7,42 +7,73 @@ import java.awt.event.WindowEvent;
 
 import javax.swing.*;
 
-public class Login implements ActionListener {
+public class Login extends JFrame implements ActionListener {
 
-    JFrame frame = new JFrame();
-    JPanel panel = new JPanel(new GridLayout(3, 1));
+    JPanel panel;
     JLabel user_label, password_label, message;
     JTextField userName_text;
     JPasswordField password_text;
     JButton submit, cancel;
 
     Login() {
-        frame.setSize(430,230);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setTitle("Login");
+        setSize(430,230);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        GridBagConstraints gbc;
 
-        panel.setLayout(null);
-//        panel.setMaximumSize(new Dimension(215, 115));
-        frame.add(panel, BorderLayout.CENTER);
+        panel = new JPanel();
+        panel.setLayout(new GridBagLayout());
+        add(panel);
 
+        gbc = new GridBagConstraints();
+        gbc.gridx = 0;
+        gbc.gridy = 0;
+        gbc.anchor = GridBagConstraints.NORTHWEST;
+        gbc.insets = new Insets(132, 103, 0, 0);
         user_label = new JLabel("User");
-        user_label.setBounds(10,20,80,25);
-        panel.add(user_label);
+        panel.add(user_label, gbc);
 
+        gbc = new GridBagConstraints();
+        gbc.gridx = 1;
+        gbc.gridy = 0;
+        gbc.gridwidth = 3;
+        gbc.gridheight = 2;
+        gbc.ipadx = 100;
+        gbc.anchor = GridBagConstraints.NORTHWEST;
+        gbc.insets = new Insets(130, 38, 0, 111);
         userName_text = new JTextField();
-        userName_text.setBounds(100,20,165,25);
-        panel.add(userName_text);
+        panel.add(userName_text, gbc);
 
+        gbc = new GridBagConstraints();
+        gbc.gridx = 0;
+        gbc.gridy = 2;
+        gbc.anchor = GridBagConstraints.NORTHWEST;
+        gbc.insets = new Insets(20, 105, 0, 0);
         password_label = new JLabel("Password");
-        password_label.setBounds(10,50,80,25);
-        panel.add(password_label);
+        panel.add(password_label, gbc);
 
+        gbc = new GridBagConstraints();
+        gbc.gridx = 1;
+        gbc.gridy = 2;
+        gbc.gridwidth = 3;
+        gbc.gridheight = 2;
+        gbc.ipadx = 100;
+        gbc.anchor = GridBagConstraints.NORTHWEST;
+        gbc.insets = new Insets(18, 38, 0, 111);
         password_text = new JPasswordField();
-        password_text.setBounds(100,50,165,25);
-        panel.add(password_text);
+        panel.add(password_text, gbc);
 
-        frame.setTitle("Login");
-        frame.setVisible(true);
+        submit = new JButton("Submit");
+        gbc = new GridBagConstraints();
+        gbc.gridx = 0;
+        gbc.gridy = 4;
+        gbc.gridwidth = 2;
+        gbc.anchor = GridBagConstraints.NORTHWEST;
+        gbc.insets = new Insets(30, 200, 102, 0);
+        panel.add(submit, gbc);
 
+        pack();
+        setVisible(true);
     }
 
     public static void main(String[] args) {
